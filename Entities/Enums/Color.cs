38ab7f5989e96fefdce9fc26b6b21bed7498924d
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 19:12:56 -0300

    C# - Removendo using desnecessário

--- Entities/Enums/Color.cs
+++ Entities/Enums/Color.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppExercAbstractClass.Entities.Enums
+﻿namespace AppExercAbstractClass.Entities.Enums
 {
     enum Color
     {
