c08f787c8e96d6eb716faf48e21305f6bd5f79b1
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 19:10:00 -0300

    C# - Adicionando novas classes ao exercício

---
+++ Entities/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AppExercAbstractClass.Entities.Enums;
+
+namespace AppExercAbstractClass.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            this.Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
