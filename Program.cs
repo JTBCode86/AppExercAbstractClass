9453b9518369df606397b511f0aedecb65a2f3f3
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 18:12:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppExercAbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
